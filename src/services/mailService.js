859081a2fef4d0df1d1ff203a715600070245c80
Alexander <a89116213787@gmail.com>
2026-01-13 02:57:27 +0300

    fix: update mail service config

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -7,6 +7,9 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+    },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
