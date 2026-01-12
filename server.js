edff852fd5728546ef6a7b8636069df012b98679
Alexander <a89116213787@gmail.com>
2026-01-12 23:22:07 +0300

    ремонт почты

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { sendMail } from "./services/mailService.js";
+import { sendMail } from "./src/services/mailService.js";
 
 dotenv.config();
 
